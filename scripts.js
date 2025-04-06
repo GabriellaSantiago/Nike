23ba581fdccc76eb50ab503f899a721807324d73
Gabriella Santiago <santiagogabriella76@gmail.com>
2025-04-05 23:29:32 -0300

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
 
 let body = document.querySelector("body")
 let tenis = document.querySelector(".imagem-tenis")
-// document = html
-// querySelector = pegador / selecionador
+
 
 function mudarVisual(cor, imagem){
     
